3aa44113a15046a29048dcb55e14e71606c28726
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-12 08:59:29 +0800

    implemented new types

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ mod structs {
     pub mod html;
 }
 
-use crate::{lib::rfs::fs_to_str, structs::html::Values};
+use crate::{structs::{config::Object}};
 use lib::rfs::{import_conf, str_to_fs};
 pub use serde::{Deserialize, Serialize};
 use structs::html::{Div, Style};
-use toml::Value;
+
 
 fn main() {
     // import config
@@ -22,7 +22,7 @@ fn main() {
     let html = compile_html(conf);
 
     str_to_fs("index.html", &html);
-    test();
+
 }
 
 fn compile_html(conf: structs::config::Conf) -> String {
@@ -42,36 +42,43 @@ fn compile_html(conf: structs::config::Conf) -> String {
     let mut div = Div::new();
 
     if let Some(i) = conf.title {
-        let temp = Values::new().style(Style::h1).text(&i);
+        let temp = Object::new().style(Style::h1).text(&i);
         div.element(temp);
     };
     if let Some(i) = conf.locale {
-        let temp = Values::new().style(Style::h3).text(&i);
+        let temp = Object::new().style(Style::h3).text(&i);
         div.element(temp);
     };
     if let Some(i) = conf.author {
-        let temp = Values::new().style(Style::h4).text(&i);
+        let temp = Object::new().style(Style::h4).text(&i);
         div.element(temp);
     };
     if let Some(i) = conf.social {
         if let Some(x) = i.name {
-            let temp = Values::new().style(Style::h3).text(&x);
+            let temp = Object::new().style(Style::h3).text(&x);
             div.element(temp);
         };
     };
 
     // div derived from toml block vector (user generated)
-
     let mut yml_div = Div::new();
 
+    // for every block, generate the style and text (mandatory fields)
     for x in conf.main.unwrap().block.iter() {
-        let element = Values::new()
-            .style(x.clone().style.unwrap_or(Style::None))
-            .text(&x.clone().content.unwrap_or(String::from("TEMPLATE")))
-            .id(&x.id.clone().unwrap_or(String::new()));
+        let mut element = Object::new()
+            .style(x.style.clone().unwrap_or_default())
+            .text(x.text.as_ref().unwrap_or(&String::from("TEMPLATE")));
 
+        // if applicable, generate optional fields
+        if x.id.is_some() {
+            element.id(x.id.as_ref().unwrap());
+        };
+        if x.from_str.is_some() {
+            element.from_str(x.from_str.as_ref().unwrap());
+        }
         yml_div.element(element);
-    }
+    };
+    println!("{:?}", yml_div);
 
     // compiling divs
     format!(
@@ -79,17 +86,4 @@ fn compile_html(conf: structs::config::Conf) -> String {
         div.compile(),
         yml_div.compile(),
     )
-}
-
-fn test() {
-    let mut div = Div::new();
-
-    let element_1 = Values::new()
-        .style(Style::None)
-        .text("hewo!")
-        .id("44434343");
-
-    div.element(element_1);
-
-    println!("{:?}", div);
 }
