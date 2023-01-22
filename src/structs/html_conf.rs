6270a12db5412f7297f1d42421f5e0a128ff4d0a
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-01-22 10:24:29 +0800

    derive cloned for structures

--- src/structs/html_conf.rs
+++ src/structs/html_conf.rs
@@ -2,7 +2,7 @@ use crate::lib::rfs::{dir_remake, import_conf, str_to_fs};
 
 use super::toml_conf::Conf;
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Clone)]
 pub struct Generate {
     pub config: Conf,
     pub input: Option<String>,
@@ -10,7 +10,7 @@ pub struct Generate {
     pub method: GenMethod,
 }
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, Clone)]
 pub enum GenMethod {
     #[default]
     Default,
@@ -25,6 +25,7 @@ impl Generate {
         self.config = import_conf(config);
         self
     }
+    #[allow(dead_code)]
     pub fn input_dir(mut self, directory: &str) -> Self {
         self.input = Some(String::from(directory));
         self
@@ -34,6 +35,7 @@ impl Generate {
         self
     }
 
+    #[allow(dead_code)]
     pub fn method(mut self, method: GenMethod) -> Self {
         self.method = method;
         self
@@ -42,19 +44,19 @@ impl Generate {
     pub fn from_conf(genconf: Generate) {
         generate_html(genconf);
     }
-
-
 }
 
 pub fn generate_html(conf: Generate) {
-    //let input = conf.input.unwrap_or(String::from("config"));
-    let output = conf.ouput.unwrap_or(String::from("output"));
+    // defining output directory
+    let output = conf.ouput.clone().unwrap_or_else(|| String::from("output"));
+    //let input = conf.input.clone().unwrap_or_else(|| String::from("config"));
 
+    // recreate output directory
     dir_remake(&output);
-    let html = conf.config.to_html();
 
+    // create and move html file
     let html_dir = format!("{output}/index.html");
 
     std::fs::File::create(&html_dir).unwrap();
-    str_to_fs(&html_dir, &html);
+    str_to_fs(&html_dir, &conf.config.to_html());
 }
