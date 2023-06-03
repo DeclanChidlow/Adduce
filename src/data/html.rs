39d3eba0905ab2cbf41f6b4b3e38c494c6d332c2
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-06-03 15:20:43 +0800

    implemented error types for data and mardown

--- src/data/html.rs
+++ src/data/html.rs
@@ -1,4 +1,4 @@
-use crate::common::fs::{import_conf, str_to_fs};
+use crate::common::{fs::File, result::Error};
 
 use super::toml::Conf;
 
@@ -20,16 +20,24 @@ impl Generate {
     pub fn new() -> Self {
         Default::default()
     }
+    /*
 
-    pub fn conf_str(mut self, config: &str) -> Self {
-        match import_conf(config) {
-            Ok(a) => self.config = a,
-            Err(e) => {
-                println!("{e}")
+        pub fn conf_str(mut self, config: &str) -> Self {
+            match import_conf(config) {
+                Ok(a) => self.config = a,
+                Err(e) => {
+                    println!("{e}")
+                }
             }
+            self
         }
-        self
+    */
+    pub fn conf_str(&mut self, config: &str) -> Result<Self, Error> {
+        let file: Conf = File::from_path(config)?.toml_from_path()?;
+        self.config = file;
+        Ok(self.to_owned())
     }
+
     #[allow(dead_code)]
     pub fn input_dir(mut self, directory: &str) -> Self {
         self.input = Some(String::from(directory));
@@ -54,7 +62,8 @@ impl Generate {
     }
 }
 
-pub fn generate_html(conf: Generate) {
+pub fn generate_html(conf: Generate) -> Result<(), Error> {
+    /*
     // defining output directory
     let output = conf.ouput.clone().unwrap_or_else(|| String::from("output"));
 
@@ -74,4 +83,34 @@ pub fn generate_html(conf: Generate) {
 
     std::fs::File::create(&html_dir).unwrap();
     str_to_fs(&html_dir, &conf.config.to_html());
+    */
+
+    // defining output directory
+    let output = conf
+        .ouput
+        .as_ref()
+        .unwrap_or_else(|| &String::from("output"));
+
+    // create and move html file
+    let html_file_path = format!(
+        "{output}/{}",
+        conf.filename.unwrap_or_else(|| String::from("index.html"))
+    );
+
+    // if html directory doesnt exist; create
+    if File::from_path(&output).is_err() {
+        File::new().set_path(&output).write_directory()?;
+    }
+
+    // if html file exists, remove it
+    let file = match File::from_path(&html_file_path) {
+        Ok(file) => file,
+        Err(_) => {
+            let file = File::new().set_path(&html_file_path);
+            file
+        }
+    };
+    // write content
+    file.set_content(&conf.config.to_html()).write()?;
+    Ok(())
 }
--- src/html/md.rs
+++ src/html/md.rs
@@ -1,38 +1,43 @@
 use pulldown_cmark::{html, Parser};
 
 use crate::{
-    common::fs::fs_to_str,
-    data::toml::{Conf, Object},
+    common::{fs::File, result::Error},
+    data::toml::{Conf, Main, Object},
 };
 
 impl Conf {
-    pub fn to_html(&self) -> String {
-        let mut divs = String::new();
-        for x in self.main.clone().unwrap_or_default().block.iter() {
-            divs += &compile_html(x);
+    pub fn to_html(&self) -> Result<String, Error> {
+        // main html page
+        let mut blocks = String::new();
+        if let Some(Main { block }) = self.main {
+            for item in block {
+                blocks += &compile_html(&item)?;
+            }
         }
 
         // CSS stylesheets - not to be confused with 'style'
         let mut style_conf = String::new();
 
-        if let Some(styles) = self.clone().style {
-            for x in styles {
-                style_conf += &fs_to_str(&x);
+        if let Some(styles) = self.style {
+            for path in styles {
+                style_conf += &File::from_path(&path)?.get_content();
             }
         }
 
+        // HTML head values
         let mut head_conf = String::new();
-        if let Some(head) = self.clone().head {
-            for x in head {
-                head_conf += &fs_to_str(&x);
+        if let Some(head) = self.head {
+            for path in head {
+                head_conf += &File::from_path(&path)?.get_content();
             }
         }
-
-        format!("<!DOCTYPE html>\n<head>\n<style>\n{style_conf}\n</style>\n{head_conf}\n</head>\n<body>\n<div class=\"page\">\n{divs}\n</div>\n</body>")
+        let html =  format!("<!DOCTYPE html>\n<head>\n<style>\n{style_conf}\n</style>\n{head_conf}\n</head>\n<body>\n<div class=\"page\">\n{divs}\n</div>\n</body>");
+        Ok(html)
     }
 }
 
-fn compile_html(conf: &Object) -> String {
+pub fn compile_html(conf: &Object) -> Result<String, Error> {
+    /*
     // import configuration and own it
     let conf: Object = conf.to_owned();
 
@@ -70,19 +75,60 @@ fn compile_html(conf: &Object) -> String {
     };
 
     html
+    */
+
+    // import configuration and own it
+    let conf: Object = conf.to_owned();
+
+    // define basic style
+    let style = conf.style.unwrap_or_default();
+
+    // define text
+    let text = match conf.from_str {
+        None => conf.text.unwrap_or_else(|| String::from("PLACEHOLDER")),
+        Some(path) => File::from_path(&path)?.get_content(),
+    };
+
+    let pt_text = pretty_text(&text);
+
+    // defines id
+    let id = match conf.id {
+        None => String::new(),
+        Some(a) => format!(" id=\"{a}\""),
+    };
+
+    // mutually exclusive conditionals
+    let mut html = match &style as &str {
+        "br" => String::from("\n<br>"),
+        "html" => String::from("\n") + &pt_text,
+        "md" => md_two(&text),
+        // standard
+        _ => format!("\n<{style}{id}>{pt_text}\n</{style}>"),
+    };
+
+    // anchor
+    if let Some(link) = conf.link {
+        html = format!("\n<a href=\"{link}\">\n{html}\n</a>");
+    };
+
+    Ok(html)
 }
 
 fn md_two(text: &str) -> String {
-    let parser = Parser::new(text);
     let mut html_output = String::new();
-    html::push_html(&mut html_output, parser);
+    html::push_html(&mut html_output, Parser::new(text));
     html_output
 }
 
 fn pretty_text(text: &str) -> String {
+    /*
     let mut fin = String::new();
     for x in text.split('\n') {
         fin += &format!("\n    {x}");
     }
     fin
+    */
+    text.split('\n')
+        .map(|item| format!("\n    {item}"))
+        .collect()
 }
