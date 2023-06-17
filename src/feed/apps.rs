c4fe7a9bc391a3d1f36c161af9dbb3776a498044
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-06-17 14:53:37 +0800

    added error messages for apps

--- src/feed/apps.rs
+++ src/feed/apps.rs
@@ -1,21 +1,24 @@
 use std::process::{Child, Command};
 
-use crate::common::result::{Error, ErrorConvert};
+use crate::common::result::{Error, ErrorConvert, ErrorType};
 
 pub struct Com();
 
 impl Com {
     pub fn spawn(command: &str, args: Option<Vec<&str>>) -> Result<(), Error> {
-        Com::common(command, args)?.wait().res()?;
-        Ok(())
+        Com::common(command, args)?
+            .wait()
+            .res_ignore(Some("falied to start session for command"))
     }
     pub fn results(command: &str, args: Option<Vec<&str>>) -> Result<String, Error> {
-        let data = Com::common(command, args)?.wait_with_output().res()?;
+        let data = Com::common(command, args)?
+            .wait_with_output()
+            .res_msg(Some("failed to get results of command"))?;
 
         match (to_string_ignore(data.stdout), to_string_ignore(data.stderr)) {
-            a if a.0.is_empty() && !a.1.is_empty() => Err(Error::Command(a.1)),
+            a if a.0.is_empty() && !a.1.is_empty() => Err(ErrorType::Command(a.1).into()),
             a if a.1.is_empty() && !a.0.is_empty() => Ok(a.0),
-            _ => Err(Error::Command(String::from("illogical command error"))),
+            _ => Err(ErrorType::Command(String::from("illogical command error")).into()),
         }
     }
     pub fn common(command: &str, args: Option<Vec<&str>>) -> Result<Child, Error> {
@@ -24,7 +27,7 @@ impl Com {
         if let Some(args) = args {
             cmd.args(args);
         };
-        cmd.spawn().res()
+        cmd.spawn().res_msg(Some("failed to start command"))
     }
 }
 
