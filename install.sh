943f27a39c3dae955f03dee50b29809bb44179c1
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-05-07 10:05:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Building Adduce..."
-cargo build --release
-
 echo "Installing Adduce..."
-if [[ -f /bin/adduce ]]; then
-    sudo rm /bin/adduce
-fi
-
-sudo cp target/release/adduce /bin/
-echo "Completed!"
+cargo install adduce
+echo "Done!"
